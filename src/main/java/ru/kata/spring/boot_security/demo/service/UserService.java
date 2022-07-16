4b70743f63e3a2dff1d120dc801a101e6dea8190
I <IIgorOs>
2022-07-16 12:14:35 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,13 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import ru.kata.spring.boot_security.demo.model.User;
+import java.util.List;
+
+public interface UserService {
+    List<User> getUsers();
+    void save(User user, String[] roles, String pass);
+    User findUser(int id);
+    User findByEmail(String email);
+    void update(User user, int id, String[] roles, String pass);
+    void delete(int id);
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -0,0 +1,69 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.dao.UserDao;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserDao userDao;
+    private RoleService roleService;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao, RoleService roleService, BCryptPasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
+        this.roleService = roleService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userDao.getUsers();
+    }
+
+    @Transactional
+    @Override
+    public void save(User user, String[] roles, String pass) {
+        user.setPassword(passwordEncoder.encode(pass));
+        user.setRoles(Arrays.stream(roles)
+                .map(role -> roleService.findByRole(role))
+                .collect(Collectors.toList()));
+        userDao.save(user);
+    }
+
+    @Override
+    public User findUser(int id) {
+        return userDao.findUser(id);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userDao.findByEmail(email);
+    }
+
+    @Transactional
+    @Override
+    public void update(User user, int id, String[] roles, String pass) {
+        user.setId(id);
+        user.setPassword(passwordEncoder.encode(pass));
+        user.setRoles(Arrays.stream(roles)
+                .map(role -> roleService.findByRole(role))
+                .collect(Collectors.toList()));
+        userDao.update(user);
+    }
+
+    @Transactional
+    @Override
+    public void delete(int id) {
+        userDao.delete(id);
+    }
+
+}
